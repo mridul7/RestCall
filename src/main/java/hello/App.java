38180e2b3550f588427836db0a03ad9ba5fae07f
mridul7 <mridul.singh603@gmail.com>
2020-04-13 09:31:51 +0530

    kl

--- src/main/java/hello/App.java
+++ src/main/java/hello/App.java
@@ -10,7 +10,7 @@ public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-        System.out.println("hii");
+        System.out.println("hi there hello world !!");
     }
 
 }
